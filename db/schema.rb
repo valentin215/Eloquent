4ca8913b5071f1970991033aeb50d2f7fe335361
cristobal artigas <artigas.cristobal@gmail.com>
2019-05-28 12:33:05 +0200

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_164303) do
+ActiveRecord::Schema.define(version: 2019_05_28_102207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2019_05_27_164303) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "biography"
+    t.string "native_language"
+    t.string "photo"
+    t.string "password"
+    t.string "nationality"
+    t.float "teacher_rating"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
