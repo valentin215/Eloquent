cf481eeac73d4b13b5dc72c386e3f46acc2bf456
cristobal artigas <artigas.cristobal@gmail.com>
2019-05-29 14:41:09 +0200

    i've finnish the pick up category

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,36 @@
 class UsersController < ApplicationController
   def edit
-    @tags_by_category = InterestCategory.all.map { |c|  [c, c.interest_tags] }.to_h
+    @tags_by_category = InterestCategory.includes(:interest_tags).all.map { |c|  [c, c.interest_tags] }.to_h
   end
 
   def update
+    create_user_interests
     current_user.update(user_params)
-    redirect_to user_path(current_user)
+    redirect_to edit_user_path(current_user)
   end
 
   def show
   end
 
+  private
+
+  def create_user_interests
+    # current_user.user_interests.reject { |ui| ui.interest_tag_id.in? user_params[:interest_tag_ids }.destroy
+    interest_tag_ids = user_params[:interest_tag_ids].reject { |it| it == "" }
+
+    interest_tag_ids.each do |interest_tag_id|
+     UserInterest.create(user_id: current_user.id, interest_tag_id: interest_tag_id)
+    end
+  end
+
   def user_params
-    params.require(:user).permit(:email, :name, :biography, :native_language, :photo, :nationality)
+    params.require(:user).permit(:email,
+      :name,
+      :biography,
+      :native_language,
+      :photo,
+      :nationality,
+      interest_tag_ids: [])
   end
 
 end
