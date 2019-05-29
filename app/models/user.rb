6d8ead3d508f9cbfd5b9a2f1a2d5782fbdec0bfd
JuanBarbed <juanbarbed@gmail.com>
2019-05-29 16:14:36 +0200

    show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,23 @@ class User < ApplicationRecord
   has_many :user_languages
   has_many :languages, through: :user_languages
   has_many :bookings
-  has_many :courses, through: :bookings # FOR STUDENTS
+
+  has_many :student_courses, through: :bookings, source: 'Course' # FOR STUDENTS
   has_many :courses # FOR TEACHERS
   has_many :reviews, through: :bookings # Student review a course
   has_many :interest_tags, through: :user_interests
+  # current_user.teacher_reviews
+
+  def teacher_reviews
+    reviews = []
+    courses.each do |course|
+      course.bookings.each do |booking|
+        reviews << booking.review
+      end
+    end
+    reviews
+  end
+
 end
+
+# User.new.student_courses
