6cfcc025f062cfad5679162124ae35e7555cd7c3
Valentin Birault <biraultvalentin@gmail.com>
2019-06-05 13:13:24 +0200

    fixing method call

--- app/models/interest_category.rb
+++ app/models/interest_category.rb
@@ -1,3 +1,7 @@
 class InterestCategory < ApplicationRecord
   has_many :interest_tags
+
+  def self.interests
+    self.all.pluck(:content).uniq
+  end
 end
--- app/queries/search_courses.rb
+++ app/queries/search_courses.rb
@@ -10,10 +10,20 @@ class SearchCourses
     @scope = filter_by_level(@scope, params[:level]) if params[:level].present?
     @scope = filter_by_language(@scope, params[:language]) if params[:language].present?
     @scope = filter_by_city(@scope, params[:city]) if params[:city].present?
+    @scope = filter_by_categories(@scope, params[:categories]) if params[:categories].present?
+    # @scope = filter_by_tags(@scope, params[:tags]) if params[:tags].present?
     @scope
   end
 
   private
+
+  def filter_by_categories(scope, categories)
+    scope.where('categories @@ ?', categories)
+  end
+
+  def filter_by_tags(scope, tags)
+    scope.where('tags @@ ?', tags)
+  end
 
   def filter_by_city(scope, city)
     scope.near(city)
@@ -27,7 +37,6 @@ class SearchCourses
     scope.where('level @@ ?', level)
   end
 end
-
 
     # @courses = Course.all
 
