84238d56de08f359eb132ea2782f60123e578e22
JuanBarbed <juanbarbed@gmail.com>
2019-06-05 10:35:02 +0200

    recuperate-map-index

--- db/migrate/20190604161207_add_start_time_to_courses.rb
+++
@@ -1,5 +0,0 @@
-class AddStartTimeToCourses < ActiveRecord::Migration[5.2]
-  def change
-    add_column :courses, :start_time, :time
-  end
-end
--- db/migrate/20190604161225_add_end_time_to_courses.rb
+++
@@ -1,5 +0,0 @@
-class AddEndTimeToCourses < ActiveRecord::Migration[5.2]
-  def change
-    add_column :courses, :end_time, :time
-  end
-end
