3e44b96b79cec208ddd6d1f54316bc2ed0ef18d7
cristobal artigas <artigas.cristobal@gmail.com>
2019-06-05 13:25:06 +0200

    fix the create course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -41,6 +41,7 @@ def create
   @course = Course.new(course_params)
   @course.user = current_user
   if @course.save
+    create_course_days
     redirect_to course_path(@course)
   else
     render :new
@@ -78,6 +79,17 @@ def destroy
 
   private
 
+  def create_course_days
+    params[:course][:course_day_ids].each do |day|
+      CourseDay.create(
+        course_id: @course.id,
+        start_time: params[:course][:start_time],
+        end_time: params[:course][:end_time],
+        working_day: day
+      )
+    end
+  end
+
   def course_params
 
     params.require(:course).permit(
@@ -95,7 +107,6 @@ def destroy
       :picture_cache,
       :capacity,
       :price,
-      :start_time,
-      :end_time)
+    )
   end
 end
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -6,3 +6,9 @@ flatpickr("#course_start_date", {
   altInput: true,
   plugins: [new rangePlugin({ input: "#course_end_date"})]
 });
+
+flatpickr(".course_time", {
+  enableTime: true,
+  noCalendar: true,
+  dateFormat: "H:i"
+})
