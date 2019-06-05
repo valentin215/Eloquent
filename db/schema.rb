0768dbd6797429bc9ab35a2872d8b508c46cfe71
Valentin Birault <biraultvalentin@gmail.com>
2019-06-05 20:45:32 +0200

    changing params query city for displaying in first page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_06_05_093755) do
 
   # These are extensions that must be enabled in order to support this database
@@ -58,9 +57,9 @@ ActiveRecord::Schema.define(version: 2019_06_05_093755) do
     t.datetime "updated_at", null: false
     t.integer "capacity"
     t.string "picture"
+    t.integer "price_cents", default: 0, null: false
     t.string "start_time"
     t.string "integer"
-    t.integer "price_cents", default: 0, null: false
     t.index ["language_id"], name: "index_courses_on_language_id"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
