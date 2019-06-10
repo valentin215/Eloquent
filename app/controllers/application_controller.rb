d53c3f55858e354d76739e2aef32f22db6a4a8bd
Valentin Birault <biraultvalentin@gmail.com>
2019-06-10 22:57:13 +0200

    test on commit working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,6 @@ def after_sign_in_path_for(resource)
   stored_location_for(resource) || edit_user_path(current_user)
 end
 
-
-
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
