3e3a820932a922ac4bb09649f77fd6d24793cd0c
Valentin Birault <biraultvalentin@gmail.com>
2020-02-16 23:06:02 +0100

    working for connectivity with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+  devise :omniauthable, omniauth_providers: [:facebook]
 
   has_many :user_interests
   has_many :user_languages
@@ -56,8 +58,8 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session[“devise.facebook_data”] && session[“devise.facebook_data”][“extra”][“raw_info”]
-        user.email = data[“email”] if user.email.blank?
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
 
+  root to: 'pages#home'
+
   get 'courses/index'
   get 'courses/show'
   get 'courses/new'
   get 'courses/edit'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root to: 'pages#home'
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
+  # get 'auth/facebook/callback', to: 'users#create'
+  # get 'auth/failure', to: redirect('/')
+
   resources :courses do
     resources :bookings, only: [:new, :create, :update]
     resources :payments, only: [:new, :create]
   end
 
   resources :users, only: [:edit, :update, :show]
-
 
   resources :bookings, only: [:show]
 
