0d6f9c63f44efeb60212e4d746c1c63dae88931b
Valentin Birault <biraultvalentin@gmail.com>
2020-02-25 21:55:50 +0100

    adding teachrer rating

--- app/queries/search_courses.rb
+++ app/queries/search_courses.rb
@@ -12,8 +12,10 @@ class SearchCourses
     @scope = filter_by_language(@scope, params[:language]) if params[:language].present?
     @scope = filter_by_city(@scope, params[:city]) if params[:city].present?
     @scope = filter_by_categories(@scope, params[:interest_categories]) if params[:interest_categories].present?
+    @scope = filter_by_teacher_rating(@scope, params[:teacher_rating]) if params [:teacher_rating].present?
+    @scope = filter_by_price(@scope, params[:price]) if params[:price].present?
+    @scope = filter_by_time(@scope, params[:time]) if params[:time].present?
     @scope
-     # @scope = filter_by_tags(@scope, params[:tags]) if para.ms[:tags].present?
   end
 
   private
@@ -27,9 +29,19 @@ class SearchCourses
   # end
 
   def filter_by_categories(scope, category_id)
-    scope.joins(user: { user_interests: :interest_tag})
-      .where(interest_tags: {interest_category_id: category_id} )
+    scope.joins(user: { user_interests: :interest_tag}).where(interest_tags: {interest_category_id: category_id} )
   end
+
+  def filter_by_teacher_rating
+    scope.joins(user: )
+  end
+
+  def filter_by_price
+  end
+
+  def filter_by_time
+  end
+
 
 # where('content @@ ?', content)
   #scope.joins(:user).pluck(:user_interests).joins(:interest_tag).joins(:interest_category).where('content @@ ?', content)
