4931345e40de9bb7b44f30ccde7aa838e0dc4c55
Valentin Birault <biraultvalentin@gmail.com>
2020-02-27 22:55:56 +0100

    adding query filter

--- app/queries/search_courses.rb
+++ app/queries/search_courses.rb
@@ -12,11 +12,11 @@ class SearchCourses
     @scope = filter_by_language(@scope, params[:language]) if params[:language].present?
     @scope = filter_by_city(@scope, params[:city]) if params[:city].present?
     @scope = filter_by_categories(@scope, params[:interest_categories]) if params[:interest_categories].present?
-    # @scope = filter_by_teacher_rating(@scope, params[:teacher_rating]) if params [:teacher_rating].present?
     @scope = filter_by_price(@scope, params[:price]) if params[:price].present?
     @scope = filter_by_time(@scope, params[:time]) if params[:time].present?
     @scope = filter_by_day(@scope, params[:day]) if params[:day].present?
     @scope = filter_by_date(@scope, params[:date]) if params[:date].present?
+    @scope = filter_by_teacher_rating(@scope, params[:teacher_rating]) if params[:teacher_rating].present?
     @scope
   end
 
@@ -26,31 +26,42 @@ class SearchCourses
     scope.joins(user: { user_interests: :interest_tag}).where(interest_tags: {interest_category_id: category_id} )
   end
 
-  # def filter_by_teacher_rating(scope, teacher_rating)
-  #   scope.joins(:user).where("teacher_rating = ?", teacher_rating)
-  # end
-
-  # def filter_by_price(scope, price)
-  #   scope.where("price <= ?", price)
-  # end
-
+  def filter_by_price(scope, price)
+    if price = "0-50"
+      scope.where("price <= ?", "50")
+    elsif price = "50-100"
+      scope.where("price >= ? and price <= ?", "50", "100")
+    elsif price = "100-150"
+      scope.where("price >= ? and price <= ?", "100", "150")
+    elsif price = "150-200"
+      scope.where("price >= ? and price <= ?", "150", "200")
+    else
+      scope.where("price >= ?", "200")
+    end 
+  end
+  
   def filter_by_time(scope, time)
-    if time == 'morning'
+    if time == 'M'
       scope.left_outer_joins(:course_days).where("start_time >= ? and end_time <= ?", "00:01", "12:00")
-    elsif time == 'afternoon'
+    elsif time == 'A'
       scope.left_outer_joins(:course_days).where("start_time >= ? and end_time <= ?", "12:00", "18:00")
     else
       scope.left_outer_joins(:course_days).where("start_time >= ? and end_time <= ?", "18:00", "23:59")
     end 
   end
+  
+  def filter_by_teacher_rating(scope, teacher_rating)
+    teacher_rating = teacher_rating.to_f
+    scope.joins(:user).where("teacher_rating = ?", teacher_rating)
+  end
 
-  # def filter_by_date
-  #   scope.where("start_date >= ? and end_date <= ?", start_date, end_date)
-  # end
+  def filter_by_date
+    scope.where("start_date >= ? and end_date <= ?", start_date, end_date)
+  end
 
-  # def filter_by_day
-  #   scope.left_outer_joins(:course_day).where("start_time >= ? and end_time <= ?", start_time, end_time)
-  # end
+  def filter_by_day
+    scope.left_outer_joins(:course_day).where("start_time >= ? and end_time <= ?", start_time, end_time)
+  end
 
   def filter_by_city(scope, city)
     scope.near(city)
