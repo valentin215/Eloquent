103fb3904c49d8a98eeaa3c615db15349cf65b26
Valentin Birault <biraultvalentin@gmail.com>
2020-03-07 13:15:40 +0100

    adding show page + creating course flow

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,7 +9,7 @@ class BookingsController < ApplicationController
   def create
     @user = current_user
     @course = Course.find(params[:course_id])
-    @booking = Booking.new(amount: @course.price, state: 'pending', user: current_user)
+    @booking = Booking.new(total_price: @course.price, state: 'pending', user: current_user)
     @booking.course = @course
     @booking.user = current_user
     # @booking.total_price = @booking.course.price.to_i * @course.bookings.count
