cad96dc2027d794e8f5c83ef4459b96b816a5dcb
Valentin Birault <biraultvalentin@gmail.com>
2020-03-11 23:06:42 +0100

    adding booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,15 +13,26 @@ class BookingsController < ApplicationController
     @booking.course = @course
     @booking.user = current_user
     # @booking.total_price = @booking.course.price.to_i * @course.bookings.count
-    @booking.save
+    if @booking.save
+      flash[:success] = "You booking has been sent !"
+      redirect_to bookings_thanks_page_booking_path(course_id: @course.id)
+      #sent email here
+    else
+      flash[:error] = "Something went wrong, please, try again !"
+      render :new
+    end 
   end
 
-  def show
-    @booking = current_user.bookings.where(state: 'paid').find(params[:id])
-    @course = @booking.course
+  # def show
+  #   @booking = current_user.bookings.where(state: 'paid').find(params[:id])
+  #   @course = @booking.course
 
+  # end
+
+  def thanks_page_booking
+    @course = Course.find(params[:course_id])
+    @booking = Booking.find_by(course_id: params[:course_id])
   end
-
 
 #----------> Params empty we don't need a permission for bookings controller
   # private
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'courses/new'
   get 'courses/edit'
 
+  get 'bookings/thanks_page_booking'
+
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   # devise_scope :user do
@@ -17,6 +19,7 @@ Rails.application.routes.draw do
   resources :courses do
     resources :bookings, only: [:new, :create, :update]
   end
+
 
   resources :users, only: [:edit, :update, :show]
 
